1ab073c7d0ba5f442b5b3237b8cb0df71e1fabc5
clarinet-player <100056739+clarinet-player@users.noreply.github.com>
2025-02-03 15:29:36 -0800

    stuff

---
+++ classes/part_base.gd
@@ -0,0 +1,33 @@
+extends StaticBody3D
+class_name Part
+
+
+@export var type : String
+@export var mass : float
+@export var health : float
+@export_file var destroy_vfx := "res://fx/default_destruction.tscn"
+@export var attributes : Dictionary
+@export var color_indexes := [0]
+
+
+func _ready():
+	load_materials()
+	if "enemy" in get_parent():
+		collision_layer = 0b10
+	else:
+		collision_layer = 0b01
+
+
+func load_materials():
+	var surface_index = 0
+	for color in color_indexes:
+		get_node("Mesh").mesh.set("surface_" + var_to_str(surface_index) + "/material", get_parent().color_scheme[color])
+		surface_index += 1
+
+
+func destroy():
+	var vfx = load(destroy_vfx).instantiate()
+	add_sibling(vfx)
+	vfx.global_position = global_position
+	get_parent().remove_child(self)
+	queue_free()
